35f337927afdc386f685bfa0db4e3e1c50811ae4
Sam Deane <sam@elegantchaos.com>
2010-03-18 16:20:13 +0000

    cleaned up dock icon stuff added observers to react to status menu and dock settings changing

--- source/application/NSApplication+DockIcon.m
+++ source/application/NSApplication+DockIcon.m
@@ -4,33 +4,43 @@
 //
 // Borrowed from sample code by Matt Gemmell, who appears to have borrowed
 // it in turn from Tony Arnold.
+//
+// See http://codesorcery.net/2008/02/06/feature-requests-versus-the-right-way-to-do-it
+// for more information.
 // --------------------------------------------------------------------------
-
-//  Licensed under Creative Commons Attribution 2.5 - http://creativecommons.org/licenses/by/2.5/
-
 
 #import <Cocoa/Cocoa.h>
 #import <Carbon/Carbon.h>
 
 @implementation NSApplication (DockIcon)
 
+-(void) bringToFront
+{
+	ProcessSerialNumber psn = { 0, kCurrentProcess };
+	SetFrontProcess(&psn);
+}
+
+- (void) bringNextProcessToFront
+{
+	ProcessSerialNumber psnx = { 0, kNoProcess };
+	GetNextProcess(&psnx);
+	SetFrontProcess(&psnx);
+}
 
 - (void)setShowsDockIcon:(BOOL)flag 
 {
-	// this should be called from the application delegate's applicationDidFinishLaunching
-	// method or from some controller object's awakeFromNib method
-	// Neat dockless hack using Carbon from http://codesorcery.net/2008/02/06/feature-requests-versus-the-right-way-to-do-it
-	if (flag) {
-		ProcessSerialNumber psn = { 0, kCurrentProcess };
-		// display dock icon
-		TransformProcessType(&psn, kProcessTransformToForegroundApplication);
-		// enable menu bar
-		SetSystemUIMode(kUIModeNormal, 0);
-		// switch to Dock.app
-		[[NSWorkspace sharedWorkspace] launchAppWithBundleIdentifier:@"com.apple.dock" options:NSWorkspaceLaunchDefault additionalEventParamDescriptor:nil launchIdentifier:nil];
-		// switch back
-		[[NSApplication sharedApplication] activateIgnoringOtherApps:YES];
-	} else {
+	NSApplicationActivationPolicy currentPolicy = [NSApp activationPolicy];
+	if (flag && (currentPolicy == NSApplicationActivationPolicyAccessory)) 
+	{
+		NSLog(@"enabling dock icon");
+		[NSApp setActivationPolicy: NSApplicationActivationPolicyRegular];
+		[self bringNextProcessToFront];
+		[self performSelector:@selector(bringToFront) withObject:nil afterDelay:0.0];
+	} 
+	
+	else if (!flag && (currentPolicy == NSApplicationActivationPolicyRegular))
+	{
+		NSLog(@"disabling dock icon");
 		NSAlert *alert = [[[NSAlert alloc] init] autorelease];
 		[alert addButtonWithTitle:@"Relaunch Now"];
 		[alert addButtonWithTitle:@"Later"];
@@ -40,7 +50,8 @@
 		[alert setInformativeText:@"Your new setting for the Dock icon won't show up until you relaunch this application."];
 		[alert setAlertStyle:NSWarningAlertStyle];
 		 NSInteger result = [alert runModal];
-		if (result == NSAlertFirstButtonReturn) {
+		if (result == NSAlertFirstButtonReturn) 
+		{
 			[NSTask launchedTaskWithLaunchPath:@"/bin/sh" arguments:[NSArray arrayWithObjects:@"-c", [NSString stringWithFormat:@"sleep 1 ; /usr/bin/open '%@'", [[NSBundle mainBundle] bundlePath]], nil]];
 			[NSApp terminate:self];
 		}
