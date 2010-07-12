7fc0333bca3f5942eed622955489a98ed229585c
Sam Deane <sam@elegantchaos.com>
2010-07-12 21:25:24 +0100

    added unit test target to the library added unit tests for NSDictionary+ECUtilities

---
+++ source/foundation/utilities/tests/NSDictionary+ECUtilitiesTests.h
@@ -0,0 +1,17 @@
+//
+//  NSDictionary+ECUtilitiesTests.h
+//  ECFoundation
+//
+//  Created by Sam Deane on 12/07/2010.
+//  Copyright 2010 Elegant Chaos. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+
+@interface NSDictionary_ECUtilitiesTests : SenTestCase 
+{
+	NSDictionary* mDictionary;
+}
+
+@end
