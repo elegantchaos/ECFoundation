da713c9828993ad1b55c3ecf0aac33ac68eea1da
Sam Deane <sam@elegantchaos.com>
2010-07-14 01:03:10 +0100

    added unit tests for NSURL utilities

---
+++ source/mac/utilities/tests/NSAppleEventDescriptor+ECUtilitiesTests.m
@@ -0,0 +1,31 @@
+// --------------------------------------------------------------------------
+//! @author Sam Deane
+//! @date 13/07/2010.
+//
+//! @file:
+//! Unit tests for the NSAppleEventDescriptor+ECUtilitiesTests.h category.
+//
+//  Copyright 2010 Sam Deane, Elegant Chaos. All rights reserved.
+// --------------------------------------------------------------------------
+
+#import "NSAppleEventDescriptor+ECUtilitiesTests.h"
+#import "NSAppleEventDescriptor+ECUtilities.h"
+
+@implementation NSAppleEventDescriptor_ECUtilitiesTests
+
+// --------------------------------------------------------------------------
+//! Set up before each test.
+// --------------------------------------------------------------------------
+
+- (void) setUp
+{
+}
+
+// --------------------------------------------------------------------------
+//! Tear down after each test.
+// --------------------------------------------------------------------------
+
+- (void) tearDown
+{
+}
+@end
