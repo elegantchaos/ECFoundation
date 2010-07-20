c580f21b1bc4707326bbfbd6e91bd301c0d68d57
Sam Deane <sam@elegantchaos.com>
2010-07-20 13:38:38 +0100

    added ECLabelValueTableController class

---
+++ source/touch/controllers/ECLabelValueTableController.h
@@ -0,0 +1,26 @@
+// --------------------------------------------------------------------------
+//! @author Sam Deane
+//! @date 20/07/2010
+//
+//  Copyright 2010 Sam Deane, Elegant Chaos. All rights reserved.
+// --------------------------------------------------------------------------
+
+@interface ECLabelValueTableController : NSObject <UITableViewDataSource, UITableViewDelegate>
+{
+	NSArray*		mData;
+}
+
+@property (retain, nonatomic) NSArray* data;
+
+@end
+
+// --------------------------------------------------------------------------
+// Data Key Constants
+// --------------------------------------------------------------------------
+
+extern NSString *const kHeaderKey;
+extern NSString *const kFooterKey;
+extern NSString *const kRowsKey;
+extern NSString *const kLabelKey;
+extern NSString *const kDetailKey;
+
---
+++ source/touch/controllers/ECLabelValueTableController.m
@@ -0,0 +1,126 @@
+// --------------------------------------------------------------------------
+//! @author sam
+//! @date 20/07/2010
+//
+//  Copyright 2010 sam, Elegant Chaos. All rights reserved.
+// --------------------------------------------------------------------------
+
+#import "ECLabelValueTableController.h"
+
+@implementation ECLabelValueTableController
+
+// --------------------------------------------------------------------------
+// Properties
+// --------------------------------------------------------------------------
+
+
+@synthesize data = mData;
+
+// --------------------------------------------------------------------------
+// Data Key Constants
+// --------------------------------------------------------------------------
+
+NSString *const kHeaderKey = @"Header";
+NSString *const kFooterKey = @"Footer";
+NSString *const kRowsKey = @"Rows";
+NSString *const kLabelKey = @"Label";
+NSString *const kDetailKey = @"Detail";
+
+
+
+// --------------------------------------------------------------------------
+//! Release references and clean up.
+// --------------------------------------------------------------------------
+
+- (void) dealloc 
+{
+	[mData release];
+	
+    [super dealloc];
+}
+
+
+#pragma mark UITableViewDataSource methods
+
+- (NSDictionary*) dataForSection: (NSUInteger) section
+{
+	NSDictionary* data = [mData objectAtIndex: section];
+	return data;
+}
+
+- (NSArray*) rowsForSection: (NSUInteger) section
+{
+	NSDictionary* data = [mData objectAtIndex: section];
+	NSArray* rows = [data valueForKey: kRowsKey];
+	return rows;
+}
+
+- (NSDictionary*) dataForRow: (NSIndexPath*) path
+{
+	NSArray* rows = [self rowsForSection: path.section];
+	NSDictionary* row = [rows objectAtIndex: path.row];
+	return row;
+}
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
+{
+	NSInteger count = mData.count;
+
+	ECDebug(LabelValueTableChannel, @"number of sections: %d", count);
+	
+	return count;
+}
+
+- (NSString *)tableView:(UITableView *)tableView titleForHeaderInSection:(NSInteger)section
+{
+	NSDictionary* data = [self dataForSection: section];
+	NSString* result = [data valueForKey: kHeaderKey];
+
+	ECDebug(LabelValueTableChannel, @"header for section %d: %@", section, result);
+
+	return result;
+}
+
+- (NSString *)tableView:(UITableView *)tableView titleForFooterInSection:(NSInteger)section
+{
+	NSDictionary* data = [self dataForSection: section];
+	NSString* result = [data valueForKey: kFooterKey];
+
+	ECDebug(LabelValueTableChannel, @"footer for section %d: %@", section, result);
+
+	return result;
+}
+
+- (NSInteger)tableView:(UITableView *)table numberOfRowsInSection:(NSInteger)section
+{
+	NSArray* rows = [self rowsForSection: section];
+	NSInteger count = [rows count];
+	
+	ECDebug(LabelValueTableChannel, @"number of rows for section %d: %d", section, count);
+
+	return count;
+}
+
+// Row display. Implementers should *always* try to reuse cells by setting each cell's reuseIdentifier and querying for available reusable cells with dequeueReusableCellWithIdentifier:
+// Cell gets various attributes set automatically based on table (separators) and data source (accessory views, editing controls)
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+	static NSString* kCellIdentifier = @"ECLabelValueCell";
+	
+	UITableViewCell* cell = [tableView dequeueReusableCellWithIdentifier: kCellIdentifier];
+	if (cell == nil)
+	{
+		cell = [[[UITableViewCell alloc] initWithStyle: UITableViewCellStyleValue1 reuseIdentifier: kCellIdentifier] autorelease];
+	}
+	
+	NSDictionary* row = [self dataForRow: indexPath];
+	cell.textLabel.text = [row objectForKey: kLabelKey];
+	cell.detailTextLabel.text = [row objectForKey: kDetailKey];
+	cell.accessoryType = UITableViewCellAccessoryDisclosureIndicator;
+	
+	return cell;
+}
+
+
+@end
