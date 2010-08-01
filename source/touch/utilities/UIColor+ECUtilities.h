13fdfe60920bdf2f69b3142d00e9da7ea5aafeee
Sam Deane <sam@elegantchaos.com>
2010-08-01 20:52:17 +0100

    added utilities

---
+++ source/touch/utilities/UIColor+ECUtilities.h
@@ -0,0 +1,16 @@
+//
+//  UIColor+ECUtilities.h
+//  ECFoundation
+//
+//  Created by Sam Deane on 01/08/2010.
+//  Copyright 2010 Elegant Chaos. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+
+@interface UIColor(ECUtilities)
+
++ (UIColor*) blueTextColor;
+
+@end
