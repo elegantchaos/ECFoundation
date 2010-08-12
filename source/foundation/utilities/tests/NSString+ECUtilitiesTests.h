00846bbbf33cdf3786b150b92753095e7bb12b71
Sam Deane <sam@elegantchaos.com>
2010-08-12 15:51:20 +0100

    added NSString unit tests

---
+++ source/foundation/utilities/tests/NSString+ECUtilitiesTests.h
@@ -0,0 +1,16 @@
+//
+//  NSDictionary+ECUtilitiesTests.h
+//  ECFoundation
+//
+//  Created by Sam Deane on 12/07/2010.
+//  Copyright 2010 Elegant Chaos. All rights reserved.
+//
+
+#import "ECTestCase.h"
+
+
+@interface NSString_ECUtilitiesTests : ECTestCase
+{
+}
+
+@end
