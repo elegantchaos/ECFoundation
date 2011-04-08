64606dbe5b6cd665f4e46884cdda6936f50714be
Sam Deane <sam@elegantchaos.com>
2011-04-08 01:14:35 +0100

    more cleanup

--- source/foundation/utilities/NSString+ECUtilities.m
+++ source/foundation/utilities/NSString+ECUtilities.m
@@ -61,4 +61,15 @@
 	return [result autorelease];
 }
 
++ (NSString*)stringWithNewUUID
+{
+    // Create a new UUID
+    CFUUIDRef uuidObj = CFUUIDCreate(nil);
+    
+    // Get the string representation of the UUID
+    NSString *newUUID = (NSString*)CFUUIDCreateString(nil, uuidObj);
+    CFRelease(uuidObj);
+    return [newUUID autorelease];
+}
+
 @end
