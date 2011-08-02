9e199f9a04d459aa605990cafcfd6ea5c85c5af8
Sam Deane <sam@elegantchaos.com>
2011-08-02 15:58:55 +0100

    adding tables sample

---
+++ samples/iOS/ECTableSample/Source/ECTableSampleViewController.h
@@ -0,0 +1,15 @@
+//
+//  ECLoggingSampleViewController.h
+//  ECLoggingSample
+//
+//  Created by Sam Deane on 28/07/2011.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ECLoggingSampleViewController : UIViewController
+
+- (IBAction)tappedShowDebugView:(id)sender;
+
+@end
