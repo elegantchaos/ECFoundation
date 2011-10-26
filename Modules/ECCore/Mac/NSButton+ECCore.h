7582c892b9612e944ea4f6b1c80be5a8d16cf608
Sam Deane <sam@elegantchaos.com>
2011-10-26 16:20:37 +0100

    added NSButton additions

---
+++ Modules/ECCore/Mac/NSButton+ECCore.h
@@ -0,0 +1,18 @@
+// --------------------------------------------------------------------------
+//! @author Sam Deane
+//! @date 26/10/2011
+//
+//  Copyright 2011 Sam Deane, Elegant Chaos. All rights reserved.
+//  This source code is distributed under the terms of Elegant Chaos's 
+//  liberal license: http://www.elegantchaos.com/license/liberal
+// --------------------------------------------------------------------------
+
+#import <Cocoa/Cocoa.h>
+
+
+@interface NSButton (ECCore)
+
+- (NSColor*)textColour;
+- (void)setTextColour:(NSColor*)textColour;
+
+@end
