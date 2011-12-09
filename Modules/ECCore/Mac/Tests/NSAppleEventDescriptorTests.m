beca113c97d13977fda0606dcbf7e8c6e1821c3d
Sam Deane <sam@elegantchaos.com>
2011-12-09 15:31:39 +0000

    moved some tests to ECCore

--- Modules/ECCore/Mac/Tests/NSAppleEventDescriptorTests.m
+++ Modules/ECCore/Mac/Tests/NSAppleEventDescriptorTests.m
@@ -8,7 +8,7 @@
 // --------------------------------------------------------------------------
 
 #import "ECTestCase.h"
-
+#import "NSAppleEventDescriptor+ECCore.h"
 
 @interface NSAppleEventDescriptorTests : ECTestCase
 
@@ -16,8 +16,53 @@
 
 @implementation NSAppleEventDescriptorTests
 
-- (void)test
+// --------------------------------------------------------------------------
+//! Test coercing descriptor into a url.
+// --------------------------------------------------------------------------
+
+- (void) testURLValue
 {
+	NSAppleEventDescriptor* desc = [NSAppleEventDescriptor descriptorWithString: @"/Applications/Preview.app"];
+	ECTestAssertNotNil(desc);
+	
+	NSURL* url = [desc urlValue];
+	ECTestAssertStringIsEqual([url path], @"/Applications/Preview.app");
+}
+
+// --------------------------------------------------------------------------
+//! Test coercing a descriptor into an NSArray of NSStrings.
+// --------------------------------------------------------------------------
+
+- (void) testStringArrayValue
+{
+	NSAppleEventDescriptor* desc = [NSAppleEventDescriptor listDescriptor];
+	ECTestAssertNotNil(desc);
+	[desc insertDescriptor: [NSAppleEventDescriptor descriptorWithString: @"/Test/1.txt"] atIndex: 1];
+	[desc insertDescriptor: [NSAppleEventDescriptor descriptorWithString: @"/Test/2.txt"] atIndex: 2];
+	ECTestAssertIntegerIsEqual([desc numberOfItems], 2);
+	
+	NSArray* array = [desc stringArrayValue];
+	ECTestAssertIntegerIsEqual([array count], 2);
+	ECTestAssertStringIsEqual([array objectAtIndex: 0], @"/Test/1.txt");
+	ECTestAssertStringIsEqual([array objectAtIndex: 1], @"/Test/2.txt");
+}
+
+// --------------------------------------------------------------------------
+//! Test coercing a descriptor into an NSArray of NSURLs.
+// --------------------------------------------------------------------------
+
+- (void) testURLArrayValue
+{
+	NSAppleEventDescriptor* desc = [NSAppleEventDescriptor listDescriptor];
+	ECTestAssertNotNil(desc);
+	[desc insertDescriptor: [NSAppleEventDescriptor descriptorWithString: @"/Test/1.txt"] atIndex: 1];
+	[desc insertDescriptor: [NSAppleEventDescriptor descriptorWithString: @"/Test/2.txt"] atIndex: 2];
+	ECTestAssertIntegerIsEqual([desc numberOfItems], 2);
+	
+	NSArray* array = [desc urlArrayValue];
+	ECTestAssertIntegerIsEqual([array count], 2);
+	ECTestAssertStringIsEqual([[array objectAtIndex: 0] path], @"/Test/1.txt");
+	ECTestAssertStringIsEqual([[array objectAtIndex: 1] path], @"/Test/2.txt");
 }
 
 @end
