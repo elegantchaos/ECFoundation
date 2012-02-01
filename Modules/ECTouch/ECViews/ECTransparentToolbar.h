ae5fae5d97c4c80ba43a6d4555f99a576fd1496a
Sam Deane <sam@elegantchaos.com>
2012-02-01 16:30:54 +0000

    added transparent toolbar

---
+++ Modules/ECTouch/ECViews/ECTransparentToolbar.h
@@ -0,0 +1,13 @@
+//
+//  ECTransparentToolbar.h
+//  AllenOvery
+//
+//  Created by Sam Deane on 01/02/2012.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ECTransparentToolbar : UIToolbar
+
+@end
---
+++ Modules/ECTouch/ECViews/ECTransparentToolbar.m
@@ -0,0 +1,18 @@
+//
+//  ECTransparentToolbar.m
+//  AllenOvery
+//
+//  Created by Sam Deane on 01/02/2012.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "ECTransparentToolbar.h"
+
+@implementation ECTransparentToolbar
+
+- (void)drawRect:(CGRect)rect
+{
+    
+}
+
+@end
