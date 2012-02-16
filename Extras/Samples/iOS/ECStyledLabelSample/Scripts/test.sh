386d279470f558a69e75e0c8db7c81ac608bb744
Sam Deane <sam@elegantchaos.com>
2012-02-16 17:56:42 +0000

    fixed sample after big rename of classes

--- Extras/Samples/iOS/ECStyledLabelSample/Scripts/test.sh
+++
@@ -1,9 +0,0 @@
-echo Testing iOS ECStyledLabelSample
-
-base=`dirname $0`
-source "$base/../../../../Scripts/shared-test.sh"
-targetIOS="ECStyledLabelSample"
-
-# build the framework
-
-xcodebuild -target "$targetIOS" $config -sdk "$sdkIOS" clean build
