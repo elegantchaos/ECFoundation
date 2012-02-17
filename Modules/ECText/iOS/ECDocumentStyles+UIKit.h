6be346d5af6497ec61b58314c015b355ce6e16df
Sam Deane <sam@elegantchaos.com>
2012-02-17 13:55:20 +0000

    fixed UIKit stuff

---
+++ Modules/ECText/iOS/ECDocumentStyles+UIKit.h
@@ -0,0 +1,37 @@
+// --------------------------------------------------------------------------
+//! @author Sam Deane
+//! @date 18/10/2011
+//
+//  Copyright 2011 Sam Deane, Elegant Chaos. All rights reserved.
+//  This source code is distributed under the terms of Elegant Chaos's 
+//  liberal license: http://www.elegantchaos.com/license/liberal
+// --------------------------------------------------------------------------
+
+#import "ECDocumentStyles.h"
+
+@class ECDocumentStyles;
+
+@interface ECDocumentStyles(UIKit)
+
+- (id)initWithFont:(UIFont*)font colour:(UIColor*)colourIn;
+
+@end
+
+@interface UILabel(ECDocumentStyles)
+
+- (ECDocumentStyles*)defaultStyles;
+
+@end
+
+@interface UITextField(ECDocumentStyles)
+
+- (ECDocumentStyles*)defaultStyles;
+
+@end
+
+@interface UITextView(ECDocumentStyles)
+
+- (ECDocumentStyles*)defaultStyles;
+
+@end
+
