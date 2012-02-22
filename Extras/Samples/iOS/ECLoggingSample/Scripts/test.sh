3fa200dfcefcdd2badf1c6be10b51f7ef649c02b
Sam Deane <sam@elegantchaos.com>
2012-02-22 23:25:20 +0000

    removed copies of logging samples from ECFoundation

--- Extras/Samples/iOS/ECLoggingSample/Scripts/test.sh
+++
@@ -1,9 +0,0 @@
-echo Testing iOS ECLoggingSample
-
-base=`dirname $0`
-source "$base/../../../../Scripts/shared-test.sh"
-targetIOS="ECLoggingSample"
-
-# build the framework
-
-xcodebuild -target "$targetIOS" $config -sdk "$sdkIOS" clean build
